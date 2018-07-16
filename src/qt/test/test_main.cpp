312ecd5186133d621a3c184102b7e6fac0c25df1
revivalist777 <40572689+revivalist777@users.noreply.github.com>
2018-07-16 09:44:29 +0800

    Update test_main.cpp

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -1,12 +1,12 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
-// Copyright (c) 2017 The AlphaCoin developers
+// Copyright (c) 2018 The Delion developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/alp-config.h"
+#include "config/delion-config.h"
 #endif
 
 #include "util.h"
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     // Don't remove this, it's needed to access
     // QCoreApplication:: in the tests
     QCoreApplication app(argc, argv);
-    app.setApplicationName("alp-qt-test");
+    app.setApplicationName("delion-qt-test");
 
     URITests test1;
     if (QTest::qExec(&test1) != 0)
